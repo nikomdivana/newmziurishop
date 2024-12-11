f2e516acfbaffb9bc0643ffc3c15bd50f54776c1
nikachaduneli3 <testig@fds.as>
2024-12-11 20:05:00 +0400

    added filters

--- mziuri_shop/products/models.py
+++ mziuri_shop/products/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    def __str__(self):
+        return  self.name
 class Product(models.Model):
     name = models.CharField(max_length=150, default='Product Name')
     description = models.TextField(default='')
@@ -7,3 +11,9 @@ class Product(models.Model):
     create_date = models.DateTimeField(auto_now_add=True)
     write_time = models.DateTimeField(auto_now=True)
     views = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL,
+                                 null=True, related_name='products')
+
+
+    def __str__(self):
+        return  self.name
--- mziuri_shop/products/views.py
+++ mziuri_shop/products/views.py
@@ -1,13 +1,38 @@
+from itertools import product
+
 from django.shortcuts import render, get_object_or_404
-from .models import  Product
+from .models import Product, Category
 
 
 def home(request):
-    products = Product.objects.all()
-    return render(request, 'home.html', {'products': products})
+    filters = dict()
+
+    product_name = request.GET.get('product_name')
+    if product_name:
+        filters['name__icontains'] = product_name
+
+    min_price = request.GET.get('min_price')
+    if min_price:
+        filters['price__gt'] = min_price
+
+    max_price = request.GET.get('max_price')
+    if max_price:
+        filters['price__lt'] = max_price
+
+    address = request.GET.get('address')
+    if address:
+        filters['address__icontains'] = address
+
+    category =  request.GET.get('category')
+    if category:
+        filters['category_id'] = category
+
+    products = Product.objects.filter(**filters)
+    categories = Category.objects.all()
+    return render(request, 'home.html', {'products': products,
+                                                              'categories': categories})
 
 
 def product_detail(request, id):
-    product = get_object_or_404(Product, id=id)
-
+    product = get_object_or_404(Product, pk=id)
     return render(request, 'product_detail.html', {'product': product})
